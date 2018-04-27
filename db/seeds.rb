d301e7ddcc306038fc17287390974a47f67caa6c
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-04-27 19:15:03 +0800

    Add environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+options = {}
+options["account_name"]  = ENV["DEMO_ACCOUNT_NAME"] || "first_company"
+options["company"]       = ENV["DEMO_COMPANY"] || "First Company"
+options["email"]         = ENV["DEMO_EMAIL"] || "admin@first_company.com"
+options["password"]      = ENV["DEMO_PASSWORD"] || "pass"
+options["first_name"]    = ENV["DEMO_FIRST_NAME"] || "Juan"
+options["last_name"]     = ENV["DEMO_LAST_NAME"] || "Dela Cruz"
 
-@sign_up = SignUp.new account_name: "first_company", company: "First Company", email: "admin@first_company.com", password: "pass",
-  first_name: "Juan", last_name: "Dela Cruz"
+
+@sign_up = SignUp.new options
 if @sign_up.process
   puts "Sign Up success"
 else
