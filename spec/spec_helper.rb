d4a47bd21b04296e66517785c51a6ea01a9f3734
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-04-28 18:53:18 +0800

    Run console only on travis environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,14 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
-require 'simplecov-console'
+
+if ENV["TEST_ENVIRONMENT"] == "travis"
+  require 'simplecov-console'
+  SimpleCov.formatter = SimpleCov.formatter = SimpleCov::Formatter::Console
+end
+
 SimpleCov.start 'rails'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
