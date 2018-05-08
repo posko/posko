a2d87a8c6fba14b649917ad6ec583b541074c3ab
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-05-08 21:33:28 +0800

    Complete CRUD of products module

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,21 +3,37 @@ class ProductsController < ApplicationController
     @products = current_account.products
   end
   def new
-    @product_creator = ProductCreator.new
+    @products = current_account.products.new
   end
   def create
-    @product_creator = ProductCreator.new product_creator_params
-    @product_creator.user = current_user
-    @product_creator.account = current_account
-    if @product_creator.process
+    @product = current_account.products.new product_params.merge(user: current_user)
+    if @product.process
       redirect_to products_path
     else
-      puts @product_creator.errors.full_messages
+      puts @product.errors.full_messages
       render "new"
     end
   end
+  def edit
+    @product = current_account.products.find(params[:id])
+  end
+  def update
+    @product = current_account.products.find(params[:id])
+    if @product.update_attributes product_params
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+  def show
+    @product = current_account.products.find(params[:id])
+  end
+  def destroy
+    @product = current_account.products.find(params[:id]).deleted_status!
+    redirect_to products_path
+  end
   private
-    def product_creator_params
-      params.require(:product_creator).permit(:title, :price, :vendor, :product_type)
+    def product_params
+      params.require(:product).permit(:title, :price, :vendor, :product_type)
     end
 end
