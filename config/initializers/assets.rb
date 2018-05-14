ef7f6d3f929f3bd4b32a05d31707b5a838080d7d
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-05-14 21:56:56 +0800

    Fix missing temporary image

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
+Rails.application.config.assets.precompile += %w( AdminLTE/user1-128x128.jpg )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
