ae7341f7dbe244d3ebb9a65e45f273a071b6e037
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-05-30 23:16:16 +0800

    Add new and actions

--- lib/generators/breadko/breadko_generator.rb
+++ lib/generators/breadko/breadko_generator.rb
@@ -3,13 +3,17 @@ class BreadkoGenerator < Rails::Generators::NamedBase
   class_option :with_datatable, type: :boolean, default: false, description: "Add datatable"
 
   argument :attributes, type: :array, default: []
+
+  # I can't make it work.
+  # hook_for :orm
   def generate_layout
     template "breadkos_controller.erb", "app/controllers/#{objects}_controller.rb"
     template "breadko_model.erb", "app/models/#{object}.rb"
 
     template "views/index.html.erb", "app/views/#{objects}/index.html.erb"
     template "views/show.html.erb", "app/views/#{objects}/show.html.erb"
-
+    template "views/new.html.erb", "app/views/#{objects}/new.html.erb"
+    template "views/_actions.html.erb", "app/views/#{objects}/_actions.html.erb"
 
     template "breadkos_controller_spec.erb", "spec/controllers/#{objects}_controller_spec.rb"
     template "breadko_model_spec.erb", "spec/models/#{object}_spec.rb"
