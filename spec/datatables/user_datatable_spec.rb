203d1f68a6c29fd910ec73f852fb177d6187a06d
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-07-31 21:08:08 +0800

    Add user datatable spec

---
+++ spec/datatables/user_datatable_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe UserDatatable do
+
+  let(:user_datatable) { UserDatatable.new(double("view", params: params), options) }
+  let(:account) { create(:account)}
+  let(:user) { create(:user, account: account) }
+  # supporting data
+  let(:params) do
+    ActionController::Parameters.new({
+      'columns': {}
+    })
+  end
+  let(:options) do
+    {
+      current_account: account
+    }
+  end
+  before { user }
+  describe '#to_json' do
+    let(:expected_json) do
+      {
+        "recordsTotal": 1,
+        "data": [ {
+          email: user.email
+        }]
+      }
+    end
+    it { expect(user_datatable.to_json).to include_json(expected_json)}
+  end
+
+end
