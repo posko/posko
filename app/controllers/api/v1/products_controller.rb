d6d91dc6f9a907fe8521c94ccf92ecfe0ebc65b1
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-08-04 00:25:00 +0800

    Modify product's endpoint to accept filters

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,28 @@ class Api::V1::ProductsController < Api::V1::ApiController
   before_action :authenticate_user
   def index
     @products = current_account.products
+    if params[:ids].present?
+      @products = @products.where(id: params[:ids])
+    end
+
+    if params[:page].present? and params[:limit].present?
+      @products = @products.page(params[:page]).per(params[:limit])
+    end
+
+    if params[:since_id].present?
+      @products = @products.where("products.id > ?",  params[:since_id])
+    end
+
+    if params[:created_at_min].present?
+      @products = @products.where("products.created_at >= ?",  params[:created_at_min])
+    end
+
+    if params[:created_at_max].present?
+      puts @products.pluck(:created_at)
+      puts params[:created_at_max]
+      @products = @products.where("products.created_at <= ?",  params[:created_at_max])
+    end
+
     render json: { products: @products }
   end
 
--- spec/requests/products_controller_spec.rb
+++ spec/requests/products_controller_spec.rb
@@ -6,15 +6,74 @@ RSpec.describe Api::V1::ProductsController, type: :request do
   let(:product) { create(:product, account: user.account) }
   let(:access_key) { user.access_keys.first }
   let(:headers) { { 'HTTP_AUTHORIZATION': basic_auth(access_key.token, access_key.auth_token) } }
+  let(:products) { create_list(:product, 3, account: account) }
+
+  before { products }
   describe 'GET /api/v1/products' do
-    it "returns list of products" do
-      create(:product, account: account)
-      get "/api/v1/products", headers: headers
-      expect(account.products.count).to eq(1)
-      expect(json).to include_json(products: [])
+    context "retrieve all products" do
+      before { get "/api/v1/products", headers: headers }
+      it "returns list of products" do
+        expect(json["products"].count).to eq(3)
+      end
     end
+
+    context "using ids" do
+      it "retrieves products by id" do
+        get "/api/v1/products", params: { ids: [products[0].id, products[1].id]}, headers: headers
+        expect(json["products"].count).to eq(2)
+      end
+    end
+
+    context "using page and limit" do
+      it "returns list of products" do
+        get "/api/v1/products", params: { limit: 2, page: 1}, headers: headers
+        expect(json["products"].count).to eq(2)
+      end
+
+      it "returns list of products" do
+        get "/api/v1/products", params: { limit: 2, page: 2}, headers: headers
+        expect(json["products"].count).to eq(1)
+      end
+    end
+
+    context "using since_id" do
+      it "returns list of products" do
+        get "/api/v1/products", params: { since_id: products[1].id}, headers: headers
+        expect(json["products"].count).to eq(1)
+      end
+    end
+
+
+    context "using created_at_min" do
+      it "returns list of products" do
+        # TODO: test this using past date
+        Timecop.freeze(Time.current + 1.day)
+        product1 = create(:product, account: account)
+        Timecop.return
+        Timecop.freeze(Time.current + 2.day)
+        product2 = create(:product, account: account)
+        Timecop.return
+        get "/api/v1/products", params: { created_at_min: product1.created_at}, headers: headers
+        expect(json["products"].count).to eq(2)
+      end
+
+      context "using created_at_max" do
+        it "returns list of products" do
+          # TODO: test this using past date
+          Timecop.freeze(Time.current - 1.day)
+          product1 = create(:product, account: account)
+          Timecop.return
+          Timecop.freeze(Time.current - 2.day)
+          product2 = create(:product, account: account)
+          Timecop.return
+          get "/api/v1/products", params: { created_at_max: product1.created_at + 1.second}, headers: headers
+          expect(json["products"].count).to eq(2)
+        end
+      end
+    end
+
   end
-  
+
   describe 'GET /api/v1/products/:id' do
     context "with existing product" do
       it "returns the product" do
