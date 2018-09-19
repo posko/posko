bbffb43ae648968ffa375085c02fd858a174b952
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-09-20 07:33:45 +0800

    Implement product importer with basic feature

---
+++ app/importers/product_importer.rb
@@ -0,0 +1,31 @@
+require 'csv'
+class ProductImporter
+  attr_reader :filepath, :account_id, :user_id
+  def initialize options={}
+    @account_id = options.fetch(:account_id)
+    @user_id = options.fetch(:user_id)
+    @filepath = options.fetch(:filepath)
+  end
+
+  def perform
+    Product.transaction do
+      CSV.foreach(filepath, headers:true) do |row|
+        account.products.create!(product_attributes row)
+      end
+    end
+  end
+
+  private
+
+  def account
+    @account ||= Account.find(account_id)
+  end
+
+  def product_attributes row={}
+    {
+      handle: row["Handle"],
+      title: row["Name"],
+      created_by_id: user_id
+    }
+  end
+end
---
+++ spec/importers/product_importer_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe ProductImporter, type: :importer do
+  let(:filepath) { 'spec/factories/csv/products.csv' }
+  let(:account) { create(:account) }
+  let(:user) { create(:user, account: account) }
+  let(:importer) { ProductImporter.new(filepath: filepath, account_id: account.id, user_id: user.id) }
+  describe '#perform' do
+    it 'imports product from csv' do
+      importer.perform
+      expect(Product.count).to eq 2
+    end
+  end
+end
