4638972225c08880dc1f4f7ad3abb7e7197248d3
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-10-16 20:10:59 +0800

    Fix rubocop offenses

--- spec/models/account_setting_spec.rb
+++ spec/models/account_setting_spec.rb
@@ -6,23 +6,17 @@ RSpec.describe AccountSetting, type: :model do
   let(:account_setting) { create(:account_setting) }
 
   describe 'Factory data creation' do
-    subject { account_setting }
-
     it 'creates data based on factory file' do
-      is_expected.to be_present
-      is_expected.to be_persisted
+      expect(account_setting).to be_present
+      expect(account_setting).to be_persisted
     end
   end
 
   describe 'Activerecord associations' do
-    subject { account_setting }
-
-    it { is_expected.to belong_to(:account) }
+    it { expect(account_setting).to belong_to(:account) }
   end
 
   describe 'Activerecord validations' do
-    subject { account_setting }
-
-    it { is_expected.to validate_presence_of(:account) }
+    it { expect(account_setting).to validate_presence_of(:account) }
   end
 end
